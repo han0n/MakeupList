e5dac22b68958ba27d3ce4fe6371be70151a3b7e
han0n <fdezcarlos@outlook.com>
2020-12-05 23:21:31 +0100

    Confeccionado de CollectionView para mostrar cada elemento del Json por su imagen, nombre y precio mediante Bindings en MainPage.xaml. - Tal vez con otros productos me de problemas el largo de la imagen, en un principio tengo idea de abarcar para cada tipo de producto los productos de una marca que ofrezca un patrón de imagen y descripción similares.

--- MakeupList/MainPage.xaml.cs
+++ MakeupList/MainPage.xaml.cs
@@ -22,47 +22,16 @@ namespace MakeupList
 
         private async void BtnLabial_Clicked(object sender, EventArgs e)
         {
-            //var request = new HttpRequestMessage();
-            //request.RequestUri = new Uri("https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline&product_type=lipstick");
-            //request.Method = HttpMethod.Get;
-
             HttpClient cliente = new HttpClient();
             string url = "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline&product_type=lipstick";
 
             HttpResponseMessage respuesta = await cliente.GetAsync(url);
             var json = respuesta.Content.ReadAsStringAsync().Result;
 
-            Product[] modelo = Product.FromJson(json);
+            Product[] pintalabios = Product.FromJson(json);
 
-            //if (respuesta.IsSuccessStatusCode)
-            //{
-              //  string content = await respuesta.Content.ReadAsStringAsync();
-                //ObservableCollection<Product> resultado = JsonConvert.DeserializeObject<ObservableCollection<Product>>(content);
-
-                ListaProductos.ItemsSource = modelo;
-            //}
+            ListaProductos.ItemsSource = pintalabios;
         }
 
-        /*
-        private async void MostrarLabiales(object sender, EventArgs e)
-        {
-            var request = new HttpRequestMessage();
-            request.RequestUri = new Uri("http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline&product_type=lipstick");
-            request.Method = HttpMethod.Get;
-            //request.Headers.Add("Accept", "application/json");
-
-            var client = new HttpClient();
-            HttpResponseMessage response = await client.SendAsync(request);
-
-            if/*(response.StatusCode == HttpStatusCode.OK)//(response.IsSuccessStatusCode)
-            {
-                string content = await response.Content.ReadAsStringAsync();
-                var resultado = JsonConvert.DeserializeObject<List<Product>>(content);
-
-                ListaProductos.ItemsSource = resultado;
-
-            }
-        }
-        */
     }
 }
